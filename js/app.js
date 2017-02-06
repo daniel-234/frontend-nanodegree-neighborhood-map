9d8837cf62cc26f3f7f1d5bab81ea21cde265394
Daniele <daniele.rb6@gmail.com>
2017-02-06 11:35:11 +0100

    feat: Add multiple markers to the map

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
+// Store the location of 5 places
+var markersPosition = [
+	{
+		lat: 39.2146448,
+		lng: 9.1125939
+	}, {
+		lat: 39.2141012,
+		lng: 9.1140706
+	}, {
+		lat: 39.2139655,
+		lng: 9.1138612
+	}, {
+		lat: 39.2145392,
+		lng: 9.1132458
+	}, {
+		lat: 39.2128275,
+		lng: 9.1150288
+	}
+];
+
 // Create a custom binding handler to interact with the
 // Google Maps API
 ko.bindingHandlers.map = {
@@ -15,36 +35,50 @@ ko.bindingHandlers.map = {
 		// Create a new map JavaScript object using the coordinates
 		// given by the position property of the ViewModel.
 		map = new google.maps.Map(element, {
-			zoom: 18,
+			zoom: 15,
 			center: data.center
 		});
 
-		// Add a marker to the map
-		marker = new google.maps.Marker({
-			// The position field of the Marker options object literal
-			// taken by the google.maps.Marker constructor specifies a
-			// LatLng identifying the location of the marker.
-			position: data.markers[0],
-			// The map field specifies the Map on which to place the
-			// marker; here the marker is attached to the map created
-			// just above.
-			map: map,
-			title: 'Marker 1'
-		});
+		// Add a marker to the map for each selected location
+		for(var i = 0; i < data.markers.length; i++) {
+			var marker = new google.maps.Marker({
+				// The position field of the Marker options object literal
+				// taken by the google.maps.Marker constructor specifies a
+				// LatLng identifying the location of the marker.
+				position: data.markers[i],
+				// The map field specifies the Map on which to place the
+				// marker; here the marker is attached to the map created
+				// just above.
+				map: map,
+				title: 'Marker ' + (i + 1)
+			});
+		}
 	}
 };
 
-// The ViewModel only contains a property for the position
-// of the marker
+// The ViewModel contains a property for the positions of the map
+// center and the markers on the map.
 var ViewModel = function() {
 	var self = this;
-
+	// Store the map center and markers positions.
 	self.position = {
-		center: { lat: 39.2151, lng: 9.1128 },
+		center: {
+			lat: 39.2151,
+			lng: 9.1128
+		},
 		markers: [
-			{ lat: 39.214645, lng: 9.112594 }
+			// {
+			// 	lat: 39.214645,
+			// 	lng: 9.112594
+			// }
 		]
 	};
+
+	// Populate the 'markers' array property in the position field with
+	// the locations selected by the user.
+	markersPosition.forEach(function(positionItem) {
+		self.position['markers'].push(positionItem);
+	});
 };
 
 // Apply the binding to activate Knockout JS when the HTML document
